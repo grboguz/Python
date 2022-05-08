b51bc3d209c2c608ab3c70c0720173a6fb12bdae
Oğuzhan Gürbüz <71009633+grboguz@users.noreply.github.com>
2022-05-08 11:36:07 +0300

    Add files via upload

---
+++ module.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# addition, subtraction, multiplication
+
+def addition(a,b,c):
+    d = a+b+c
+    return d
+
+def subtraction(a,b,c):
+    d = a-b-c
+    return d
+
+def multiplication(a,b,c):
+    d = a*b*c
+    return d
+
